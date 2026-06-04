3f5cabf1fc113bfb5850f5ab2045e2ecb80b13c7
Ashwin433 <ashwinnair332@gmail.com>
2026-06-04 14:10:08 +0530

    feat: add ResearchPaper SQLAlchemy model

---
+++ backend/app/models/paper_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text
+from app.database.db import Base
+
+
+class ResearchPaper(Base):
+    __tablename__ = "research_papers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    authors = Column(Text)
+    abstract = Column(Text)
+    published = Column(String)
+    pdf_url = Column(String)
